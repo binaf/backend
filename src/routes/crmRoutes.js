eb03a57f2fd6df6caad3a12d1eb86b1158f84fa9
HAIDARA <ousmanehaidara26@gmail.com>
2020-08-25 10:42:40 +0200

    injecter des middleware

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -2,9 +2,17 @@ const routes = (app) => {
 
     app.route('/contact') // création des routes de l'application ici 
 
-    .get((req, res) =>   // syntaxe de creation des routes
-         res.send('demande GET avec succès')) 
+    .get((req, res, next) => {   // syntaxe de creation des routes
+        // middleware
+        console.log(` Request de: ${req.originalUrl} `)
+        console.log(` Request type: ${req.method} `)
+        next(); // pour passer à la prochaine fonction !!!
+        }, (req, res, next) => {
+            res.send('demande GET avec succès');
+           }  
 
+    ) 
+    
     .post((req, res) =>   
          res.send('demande POST avec succès'));
 
